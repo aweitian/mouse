f2315096f5f9347ba364b52b71a43406856ba203
Gandor <awei.tian@qq.com>
2019-03-04 14:19:17 +0800

    v1.0

---
+++ mouse.cpp
@@ -0,0 +1,66 @@
+// mouse.cpp : Defines the entry point for the console application.
+//
+#include "stdafx.h"
+#include <stdlib.h>
+#include<iostream>
+#include<windows.h>
+#include<winuser.h>
+using namespace std;
+
+void help()
+{
+	cout << "mouse move control version 1.0.0 / 2019-03-04" << endl;
+	cout << "Usage:" << endl;
+	cout << "  mouse move x y" << endl;
+	cout << "  mouse click x y" << endl;
+	cout << "  mouse dbclick x y" << endl;
+	cout << "  mouse rgtclick x y" << endl;
+	cout << "        source (https://github.com/aweitian/mouse)" << endl;
+}
+
+int main(int argc, char *argv[])
+{
+	int x,y;
+	if(argc == 4)
+	{
+		if(strcmp("move",argv[1]) == 0)
+		{
+			x = atoi(argv[2]);
+			y = atoi(argv[3]);
+			SetCursorPos(x,y);
+			return 0;
+		}
+		else if(strcmp("click",argv[1]) == 0)
+		{
+			x = atoi(argv[2]);
+			y = atoi(argv[3]);
+			SetCursorPos(x,y);
+			mouse_event(MOUSEEVENTF_LEFTDOWN,x,y,0,0);
+			mouse_event(MOUSEEVENTF_LEFTUP,x,y,0,0);
+			return 0;
+		}
+		else if(strcmp("dbclick",argv[1]) == 0)
+		{
+			x = atoi(argv[2]);
+			y = atoi(argv[3]);
+			SetCursorPos(x,y);
+			mouse_event(MOUSEEVENTF_LEFTDOWN,x,y,0,0);
+			mouse_event(MOUSEEVENTF_LEFTUP,x,y,0,0);
+			mouse_event(MOUSEEVENTF_LEFTDOWN,x,y,0,0);
+			mouse_event(MOUSEEVENTF_LEFTUP,x,y,0,0);
+			return 0;
+		}
+		else if(strcmp("rgtclick",argv[1]) == 0)
+		{
+			x = atoi(argv[2]);
+			y = atoi(argv[3]);
+			SetCursorPos(x,y);
+			mouse_event(MOUSEEVENTF_RIGHTDOWN,x,y,0,0);
+			mouse_event(MOUSEEVENTF_RIGHTUP,x,y,0,0);
+			return 0;
+		}
+	} 
+	help();
+	return 0;
+}
+
